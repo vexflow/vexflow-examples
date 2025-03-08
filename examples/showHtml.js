016ca5b3112b9c04810374d08116086172819edb
Ben Eidson <b.e.eidson@gmail.com>
2025-03-08 12:40:03 -0500

    part two of adding basic interaction examples, css, reorganized examples directory, and moved showHtml to its own file

---
+++ examples/showHtml.js
@@ -0,0 +1,45 @@
+function sanitizeId(id) {
+  // Replace slashes (or any other problematic characters) with dashes
+  return id.replace(/\//g, '-');
+}
+
+function showHtml(file, start, end) {
+  // Create a safe version of the file name for use in element IDs
+  const safeId = sanitizeId(file);
+
+  const fileHtml = `${file}.html`;
+  const fileMd = `${file}.md`;
+
+    // Use .content as the target container
+    const $target = $('.content');
+
+  // Use safeId for element IDs
+  $(`<div id='${safeId}_md'></div>`).appendTo($target);
+  $(`<iframe width="100%" height="250" src="${fileHtml}" title="${file}"></iframe>`).appendTo($target);
+  $(`<form action="https://jsfiddle.net/api/post/library/pure/" method="post" target="_blank">
+      <textarea hidden type="text" id="${safeId}_jsfiddle_html" name="html"></textarea><br>
+      <input type="submit" value="Edit in JSFiddle">
+    </form>`).appendTo($target);
+  $(`<div id='${safeId}_html'></div>`).appendTo($target);
+
+  $.get(fileHtml, (content) => {
+    $(`#${safeId}_jsfiddle_html`).val(content);
+    $(`#${safeId}_html`).html(window.markdownit().render(
+      `\`\`\`html\n${content.split('\n').slice(start, end).join('\n')}\n\`\`\``
+    ));
+    $(`#${safeId}_html pre code`).each((index, element) => {
+      hljs.highlightElement(element);
+    });
+  });
+
+  $.get(fileMd)
+    .done((content) => {
+      $(`#${safeId}_md`).html(window.markdownit().render(content));
+      $(`#${safeId}_md pre code`).each((index, element) => {
+        hljs.highlightElement(element);
+      });
+    })
+    .fail(() => {
+      $(`#${safeId}_md`).html(`<h2>${file}</h2>`);
+    });
+}
