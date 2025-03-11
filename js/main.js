779d4c98d337269ff85d5863c94494b360b0c87d
Ben Eidson <b.e.eidson@gmail.com>
2025-03-11 12:36:37 -0400

    added basic comments to headers. Reorganized headers to follow uniform pattern. Added entry js to main.js and fixed bug in node/font-load-1

--- js/main.js
+++ js/main.js
@@ -107,3 +107,21 @@ function showHtml(file, containerClass, start, end) {
 function showNavigation(containerClass){
     $(`.${containerClass}`).load('/partials/nav.html');
 }
+
+// used in entry demos:
+let codeArray;
+function showCode(...c) {
+  codeArray = c;
+
+  for (let i = 0; i < c.length; i++) {
+    document.getElementById('code' + i).textContent = c[i];
+  }
+}
+
+function copyCode(index) {
+  navigator.clipboard.writeText(codeArray[index]);
+}
+
+function runCode(index) {
+  eval('"use strict";' + codeArray[index]);
+}
